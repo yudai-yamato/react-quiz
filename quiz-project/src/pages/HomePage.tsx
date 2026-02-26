9cef597a47cd0afebd5a055e8e68da2fb68b592c
yudai-yamato <shimoyu15@gmail.com>
2026-02-26 15:36:16 +0900

    implement top page with start button

--- quiz-project/src/pages/HomePage.tsx
+++ quiz-project/src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
-
+import { Link } from "react-router-dom";  
+import { ROUTES } from "../const";
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <h1>Quiz App</h1>
+      <Link to={ROUTES.QUIZ}>Start!</Link>
+    </>
+
   )
 }
