4489ff77d4b74277ad2bcff82932c5ba052efd1a
yudai-yamato <shimoyu15@gmail.com>
2026-02-28 16:28:54 +0900

    feat: 問題がなくなったらRESULTページへ遷移

--- quiz-project/src/data/quiz.ts
+++ quiz-project/src/data/quiz.ts
@@ -2,27 +2,27 @@ const quizData = [
     {
         question:"Question1",
         options :["Select 1","Select 2","Select 3","Select 4"],
-        answer:0
+        answerIndex:0
     },
     {
         question:"Question2",
         options :["Select 1","Select 2","Select 3","Select 4"],
-        answer:1
+        answerIndex:1
     },
     {
         question:"Question3",
         options :["Select 1","Select 2","Select 3","Select 4"],
-        answer:2
+        answerIndex:2
     },
     {
         question:"Question4",
         options :["Select 1","Select 2","Select 3","Select 4"],
-        answer:3
+        answerIndex:3
     },
     {
         question:"Question5",
         options :["Select 1","Select 2","Select 3","Select 4"],
-        answer:0
+        answerIndex:0
     },
 ]
 
--- quiz-project/src/pages/QuizPage.tsx
+++ quiz-project/src/pages/QuizPage.tsx
@@ -1,22 +1,51 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Display from "../components/Display/Display";
 import quizData from "../data/quiz";
 import Button from "../Button/Button";
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
-  const quizIndex = 0;
+
+  const [quizIndex, setQuizIndex] = useState(0);
+  const [answerLogs, setAnswerLogs] = useState<boolean[]>([]);
+  const navigation = useNavigate();
+  const MAX_QUIZ_LEN = quizData.length;
+
+
   const handleClick = (clickedIndex: number) => {
-    console.log("clickedIndex:", clickedIndex);
+    if (clickedIndex === quizData[quizIndex].answerIndex){
+      setAnswerLogs((prev) => [...prev, true]);
+    }else {
+      setAnswerLogs((prev) => [...prev, false]);
+    }
+    setQuizIndex((prev) => prev + 1);
   };
+
+  useEffect(() => {
+    if (answerLogs.length === MAX_QUIZ_LEN) {
+      const correctNum = answerLogs.filter((answer) => {
+        return answer === true;
+      })
+      navigation(ROUTES.RESULT, {
+        state: {
+          maxQuizLen: MAX_QUIZ_LEN,
+          correctNum: correctNum.length
+        }
+      });
+    }
+  }, [answerLogs])
+
 
   return (
     <div>
-      <Display>
-        {`Q${quizIndex + 1}.${quizData[quizIndex].question}`}
-      </Display>
-      {
+      {quizData[quizIndex] && <Display>
+        {`Q${quizIndex+1}.${quizData[quizIndex].question}`}
+      </Display>}
+      {quizData[quizIndex] &&
         quizData[quizIndex].options.map((option, index) => {
           return (
-            <Button key={index} onClick={() => handleClick(index)}>
+            <Button key={index} onClick={() => {handleClick(index)}}>
               {option}
             </Button>)
         })
