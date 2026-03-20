c2f40dbb707fcf402ee3dc40792a24c67be32b27
yudai-yamato <shimoyu15@gmail.com>
2026-03-20 20:04:46 +0900

    feat:調整

--- quiz-project/src/pages/ResultPage.tsx
+++ quiz-project/src/pages/ResultPage.tsx
@@ -11,7 +11,7 @@ export default function ResultPage() {
       <h1>Result</h1>
       <p>あなたの正解数は...</p>
       <p>{`全${maxQuizLen}問中、${correctNumLen}問正解しました。`}</p>
-      <Link to={ROUTES.QUIZ}>もう一度挑戦する</Link>
+      <Link to={ROUTES.HOME}>もう一度挑戦する</Link>
     </>
 
   )
